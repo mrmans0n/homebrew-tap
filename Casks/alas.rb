e94d8b3f3a88f2e8003f11f82ac725184f3d7836
Nacho Lopez <nacho.lopez@mini.lan>
2026-05-22 01:10:45 +0200

    Fix alas cask stanza grouping

--- Casks/alas.rb
+++ Casks/alas.rb
@@ -3,10 +3,12 @@ cask "alas" do
 
   on_arm do
     sha256 "ec3476cb11083e697229d4bf0b53fc6624cca0c1eeb8d7dc11f78b762e3096cb"
+
     url "https://github.com/mrmans0n/alas/releases/download/v#{version}/Alas-#{version}-arm64.dmg"
   end
   on_intel do
     sha256 "09cb35be4ce18198545a4c44ff30e8c6b2f8234b29c81ae4cf7fade970c01701"
+
     url "https://github.com/mrmans0n/alas/releases/download/v#{version}/Alas-#{version}-x86_64.dmg"
   end
 
